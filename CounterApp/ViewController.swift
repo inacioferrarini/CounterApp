6e5b49cf0f56f5e4d7cd76a32f59784dcee4625c
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2024-07-31 12:30:16 -0300

    Initial Commit

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    private var counter: Int = 0
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateCounterLabel()
     }
 
+    @IBAction func incrementButtonTapped(_ sender: UIButton) {
+        counter += 1
+        updateCounterLabel()
+    }
+
+    @IBAction func decrementButtonTapped(_ sender: UIButton) {
+        counter -= 1
+        updateCounterLabel()
+    }
+
+    private func updateCounterLabel() {
+        counterLabel.text = "\(counter)"
+    }
 
 }
-
