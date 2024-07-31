769f1fa5a67e66cf569e2f9dd208ab128cc365da
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2024-07-31 11:09:30 -0300

    Initial Commit

---
+++ CounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by José Inácio Athayde Ferrarini on 31/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
